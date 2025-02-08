566658a50bd19b117e863070d472424f716fd0c9
ARelaxedScholar <angemmanuel.kouakou+professional@gmail.com>
2025-02-08 17:35:08 -0500

    feat(wgpu): Almost got a window opened\n This panics as of right now. :/

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,192 @@
-use glfw::{fail_on_errors, Action, Context, Key};
+#![warn(clippy::all, clippy::pedantic)]
 
-fn main() {
+use glfw::{fail_on_errors, Action, Context, Key, Window, WindowEvent};
+use wgpu::{self, rwh::HasDisplayHandle, Backends, InstanceDescriptor, Surface};
+
+struct State<'a> {
+    surface: wgpu::Surface<'a>,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    size: (i32, i32),
+    window: &'a mut Window,
+}
+
+impl<'a> State<'a> {
+    async fn new(window: &'a mut Window) -> State<'a> {
+        let size = window.get_size();
+
+        let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+            backends: wgpu::Backends::all(),
+            ..Default::default()
+        });
+        let target = unsafe { wgpu::SurfaceTargetUnsafe::from_window(window) }
+            .expect("Failed to get target");
+        let surface =
+            unsafe { instance.create_surface_unsafe(target) }.expect("Failed to create surface");
+
+        let adapter = instance
+            .request_adapter(&wgpu::RequestAdapterOptionsBase {
+                power_preference: wgpu::PowerPreference::default(),
+                force_fallback_adapter: false,
+                compatible_surface: Some(&surface),
+            })
+            .await
+            .expect("Failed to create adapter");
+
+        let (device, queue) = adapter
+            .request_device(
+                &wgpu::DeviceDescriptor {
+                    required_features: wgpu::Features::empty(),
+                    required_limits: if cfg!(target_arch = "wasm32") {
+                        wgpu::Limits::downlevel_webgl2_defaults()
+                    } else {
+                        wgpu::Limits::default()
+                    },
+                    label: None,
+                    memory_hints: Default::default(),
+                },
+                None,
+            )
+            .await
+            .expect("Failed to get device & queue.");
+
+        let surface_caps = surface.get_capabilities(&adapter);
+
+        let surface_format = surface_caps
+            .formats
+            .iter()
+            .find(|format| format.is_srgb())
+            .copied()
+            .unwrap_or(surface_caps.formats[0]);
+
+        let config = wgpu::SurfaceConfiguration {
+            usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+            format: surface_format,
+            width: size.0.max(1) as u32,
+            height: size.1.max(1) as u32,
+            present_mode: surface_caps.present_modes[0],
+            alpha_mode: surface_caps.alpha_modes[0],
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+        };
+
+        surface.configure(&device, &config);
+
+        Self {
+            surface,
+            device,
+            queue,
+            config,
+            size,
+            window,
+        }
+    }
+
+    pub fn window(&self) -> &Window {
+        &self.window
+    }
+
+    pub fn window_mut(&mut self) -> &mut Window {
+        &mut self.window
+    }
+
+    fn resize(&mut self, new_size: (i32, i32)) {
+        if new_size.0 > 0 && new_size.1 > 0 {
+            self.size = new_size;
+            self.config.width = new_size.0 as u32;
+            self.config.height = new_size.1 as u32;
+            self.surface.configure(&self.device, &self.config);
+        }
+    }
+
+    fn input(&mut self, event: &WindowEvent) -> bool {
+        false
+    }
+
+    fn update(&mut self) {}
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        let output = self.surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
+
+        let render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("Render Pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: &view,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color {
+                        r: 0.1,
+                        g: 0.2,
+                        b: 0.3,
+                        a: 1.0,
+                    }),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            depth_stencil_attachment: None,
+            timestamp_writes: None,
+            occlusion_query_set: None,
+        });
+        drop(render_pass);
+
+        // submit will accept anything
+        self.queue.submit(std::iter::once(encoder.finish()));
+        output.present();
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
     // glfw code
-    let mut glfw = glfw::init(fail_on_errors!()).unwrap();
+    let mut glfw = glfw::init(fail_on_errors!()).expect("Failed to get glfw");
 
     let (mut window, events) = glfw
         .create_window(800, 600, "wGPU training arc", glfw::WindowMode::Windowed)
-        .unwrap();
+        .expect("Failed to get window and events");
 
     window.set_key_polling(true);
-
     window.make_current();
 
-    while !window.should_close() {
+    let mut state = State::new(&mut window).await;
+
+    while !state.window.should_close() {
         glfw.poll_events();
         for (_, event) in glfw::flush_messages(&events) {
             match event {
                 glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-                    window.set_should_close(true)
+                    state.window.set_should_close(true)
                 }
-                _ => {}
+                event => {
+                    println!("{:?}", event);
+                }
+            }
+
+            state.update();
+            match state.render() {
+                Ok(_) => {}
+                Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                    state.resize(state.size)
+                }
+                Err(wgpu::SurfaceError::OutOfMemory) => {
+                    state.window.set_should_close(true);
+                    todo!("tracing as well")
+                }
+                Err(wgpu::SurfaceError::Timeout) => {
+                    todo!("tracing")
+                }
+                Err(wgpu::SurfaceError::Other) => eprintln!("Well shit"),
             }
         }
-        window.swap_buffers();
+        state.window.swap_buffers();
     }
 }
